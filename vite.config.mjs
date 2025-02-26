ab13127e6707cb2e51c9b2d1b60c70c2982a26c5
Lord_mahor <edvinkarter@gmail.com>
2025-02-26 04:06:39 +0400

    Update

--- vite.config.mjs
+++ vite.config.mjs
@@ -43,7 +43,7 @@ export default defineConfig({
                 ]
             },
             workbox: {
-                importScripts: ["/public/service-worker.js"]
+                importScripts: ["/service-worker.js"]
             },
             devOptions: {
                 enabled: true

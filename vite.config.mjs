8417f78bc5ebb3b01986935c541719536d3f2505
Lord_mahor <edvinkarter@gmail.com>
2025-03-01 02:58:56 +0400

    prod1

--- vite.config.mjs
+++ vite.config.mjs
@@ -87,7 +87,7 @@ export default defineConfig({
         outDir: "dist",
         rollupOptions: {
             input: {
-                index: "./index.html",
+                main: "./index.html",
                 "service-worker2": "./src/service-worker2.ts"
             },
             output: {
